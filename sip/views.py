7f86c9fc8caddd8e18d7879ae64f9267428f71d9
ChafikHadjAbdouRazack <yazassr@gmail.com>
2019-03-26 22:01:11 +0100

    Asterisk WebApp Manager

--- sip/views.py
+++ sip/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import telnetlib
 # Create your views here.
 
 def index (request):
@@ -8,4 +9,19 @@ def index (request):
 
 
 def login(request):
+   HOST = "192.168.1.2"
+   PORT = "5038"
+
+   tn = telnetlib.Telnet(HOST, PORT)
+   tn.write(b'Action:Login\r\n')
+   tn.write(b'Username:master\r\n')
+   tn.write(b'Secret:razack\r\n')
+   tn.write(b'\r\n')
+   tn.write(b'Action:LogOff\r\n')
+   tn.write(b'\r\n\r\n')
+   response = tn.read_all()
+   response = response.decode("UTF-8")
+   print(response)
+   resp = "Authentication accepted" in response
+   print(resp)
    pass
